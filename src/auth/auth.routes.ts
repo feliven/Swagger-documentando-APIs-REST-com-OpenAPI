15281aef9ce4c2895ac55575b4ccf8b89d64f9dd
FVS-MBA <feliven@gmail.com>
2026-05-29 18:56:46 -0300

    adiciona documentação em auth.routes

--- src/auth/auth.routes.ts
+++ src/auth/auth.routes.ts
@@ -4,12 +4,89 @@ import { userService } from "../user/user.service";
 
 const authRouter = express.Router();
 
+/**
+ * @swagger
+ * /auth/register:
+ *   post:
+ *     summary: Create a user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       "201":
+ *         description: Usuário cadastrado com sucesso
+ *         content:
+ *           text/html:
+ *             schema:
+ *               type: string
+ *       "500":
+ *         description: Internal Server Error
+ *         content:
+ *           text/html:
+ *             schema:
+ *               type: string
+ *     tags:
+ *       - Auth
+ */
+
 authRouter.post("/register", async (req: Request, res: Response) => {
   const user = req.body;
   const result = await userService.createUser(user);
   res.status(201).send(result);
 });
 
+/**
+ * @swagger
+ *   "/auth/login":
+ *   post:
+ *     summary: Log in user
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               username:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       "200":
+ *         description: Usuário logado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 token:
+ *                   type: string
+ *       "401":
+ *         description: "Error: Unauthorized"
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       "500":
+ *         description: Internal Server Error
+ *         content:
+ *           text/html:
+ *             schema:
+ *               type: string
+ *     tags:
+ *       - Auth
+ */
 authRouter.post("/login", async (req: Request, res: Response) => {
   const { username, password } = req.body;
 
